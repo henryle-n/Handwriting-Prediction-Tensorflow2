47aa95ef1ca28dfb89eb0ab06d61ba82f5a89c5d
Henry Le <henryle.b11@gmail.com>
2020-07-19 10:05:28 -0500

    flask building

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 # Author: Henry Le
 # Date: Jul. 18, 2020
 # version 0 - first release
-# if you some how find this file of mine on github, please feel free to take and modify it. I wish you the best of luck on what you want to do.
+# if you somehow find this file of mine on github, please feel free to take and modify it. 
+# I wish you the best of luck on what you want to do.
 # learning coding is not easy as it's about learning a new language but definitely FUN FUN FUN !!!
-# HAVE FUN !
+# SO, HAVE FUN !
 
 # dependencies
 from  predict import img_predict
 from flask import Flask, jsonify, render_template, request
-import tensorflow as tf
 
 
 ################## Flask App set up###############
@@ -26,19 +26,25 @@ def home_page():
 # country count per year
 @app.route("/prediction", methods=["GET", "POST"])
 def predict():
+    '''
+    create a 2 way communication channels between client and server
+    GET , POST method to take incoming data from client
+    '''
     incoming_pkg = request.get_json()
-    # author = incoming_pkg['author']
-    # app.logger.debug(f"This is the author ::\n {author}")
-    app.logger.debug(f"This is the imc pkg ::\n {incoming_pkg}")
-    # app.logger.debug('================================')
-    # img_b64_str_encoded = request.get_json()['imgBase64']
-    # app.logger.debug(f"This is the img data ::\n {img_b64_str_encoded}")
-    app.logger.debug("request made")
-    result = "is ran already"
-    return jsonify(result)
+    author = incoming_pkg['author']
+    img_b64_str_encoded = incoming_pkg['imgBase64']
 
+    # app.logger.debug(f"This is the imc pkg ::\n {incoming_pkg}")
+    # app.logger.debug(f"This is the author ::\n {author}")
+    # app.logger.debug(f"This is the img pk::\n {img_b64_str_encoded}")
+    
+    result = img_predict(img_b64_str_encoded)
+    app.logger.debug("this is number what?", result)
+    print ("this is the result :: \n", result[0])
+    r
+    done = "hey it's done"
+    return done
 
 # if program is run from this file ::
 if __name__ == '__main__':
-    print(tf.__version__)
     app.run(debug=True)
